93bf4ba6deb2d4517e3ee68f1ff91c8274d54652
Χριστίνα Λεγάκη <t8230074@aueb.gr>
2024-11-13 13:26:14 +0200

    Πορσθήκη τησ εντολής System.out.println

--- A.java
+++ A.java
@@ -2,5 +2,6 @@ public class A {
     public static void main(String[] args) {
         System.out.println("t8230105");
         System.out.println("t8230154");
+        System.out.println("t8230074");
     }
 }
