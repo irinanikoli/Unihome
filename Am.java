6809da7d4bc93f1a8cd430fbd021303a1ea4a535
Dimitra Liakopoulou”git config --global user.email t8230076@aueb.gr” <dimitraliakopoulou@Air-Dimitra.router.wind>
2024-11-13 14:54:36 +0200

    Προσθήκη System.out.println

--- Am.java
+++ Am.java
@@ -5,5 +5,6 @@ public class Am {
         System.out.println("t8230154");
         System.out.println("t8230074");
         System.out.println("t8230003");
+        System.out.println("t8230076");
     }
 }
