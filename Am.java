33e3c6cbf351d317e0385010a90e323e15b613ff
= <t8230154@aueb.gr>
2024-11-24 11:24:40 +0200

    deleted non needed files 1

--- Am.java
+++
@@ -1,9 +0,0 @@
-public class Am {
-    public static void main(String[] args) {
-        // we playing // 
-        System.out.println("t8230105");
-        System.out.println("t8230154");
-        System.out.println("t8230074");
-        System.out.println("t8230003");
-    }
-}
