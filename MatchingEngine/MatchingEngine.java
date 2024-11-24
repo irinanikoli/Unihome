49c5b600ebff104a5cc06a95205441f2aa7ad4cb
= <t8230154@aueb.gr>
2024-11-24 14:36:59 +0200

    Initial utility of MatchingEngine

--- MatchingEngine/MatchingEngine.java
+++ MatchingEngine/MatchingEngine.java
@@ -1,18 +1,47 @@
 import io.jenetics.*;
 import io.jenetics.engine.*;
 import java.util.List;
+import java.util.Map;
 
 public class MatchingEngine {
     private List<HousingOption> housingOptions; 
-    public static final int topN = 10;
+    private Map<String, Double> weights;
+    public static final int TOP_N = 10;
     // number of houses to return
-    public MatchingEngine(List<HousingOption> housingOptions) {
+    public MatchingEngine(List<HousingOption> housingOptions, Map<String, Double> weights) {
         this.housingOptions = housingOptions;
+        this.weights = weights;
     }
-    public double evaluate(HousingOption ho, Student preferences) {
+    /**
+     * Calculate the score of a house based on weights and its characteristics
+     */
 
-
+    public double evaluate(HousingOption ho) {
+        double score = 0.0;
+        score += weights.getOrDefault("cost", 0.0) * (1.0 / ho.getCost());
+        score += weights.getOrDefault("size", 0.0) * ho.getSize();
+        score += weights.getOrDefault("distanceFromUni", 0.0) * ho.getDistanceFromUni();
+        score += weights.getOrDefault("distanceFromMeans", 0.0) * ho.getDistanceFromMeans();
+        return score;
     }
-    
-    
+
+   /**
+    * Optimize and return the best house
+    */
+   
+    public HousingOption optimize() {
+        Engine<DoubleGene, Double> engine = Engine.builder(
+            individual -> evaluate(housingOptions.get((int) Math.floor(individual.getdoubleValue()))),
+            Codecs.ofScalar(DoubleRange.of(0, housingOptions.size()))
+        ).build();
+
+        Phenotype<DoubleGene, Double> best = engine.stream()
+                    .limit(100)
+                    .collect(EvolutionResult.toBestPhenotype());
+                    
+        return housingOptions.get(best.genotype().gene().allele().intValue());
+         
+        
+    }
+
 }
