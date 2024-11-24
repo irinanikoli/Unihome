fb6d986f78cc147856180d7ace4046c9d773f8b0
= <t8230154@aueb.gr>
2024-11-24 12:45:15 +0200

    Creation of WeightCalculator.java

--- MatchingEngine/MatchingEngine.java
+++ MatchingEngine/MatchingEngine.java
@@ -4,8 +4,15 @@ import java.util.List;
 
 public class MatchingEngine {
     private List<HousingOption> housingOptions; 
+    public static final int topN = 10;
+    // number of houses to return
     public MatchingEngine(List<HousingOption> housingOptions) {
         this.housingOptions = housingOptions;
     }
+    public double evaluate(HousingOption ho, Student preferences) {
+
+
+    }
+    
     
 }
---
+++ MatchingEngine/WeightCalculator.java
@@ -0,0 +1,16 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.stream.*;
+import java.util.List;
+
+
+public class WeightCalculator {
+    /**
+     *  calculates the weigths of students' preferences
+     * 
+     * @param priorities Map of priorities (1= higher, n = lower)
+     * @return Map of weights for each parameter
+     */
+    
+
+}
