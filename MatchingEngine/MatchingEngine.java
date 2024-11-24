4469a8c3f1507f38bcba4be67e8b0733f8101e05
= <t8230154@aueb.gr>
2024-11-24 11:28:53 +0200

    MatchingEngine.java creation

---
+++ MatchingEngine/MatchingEngine.java
@@ -0,0 +1,11 @@
+import io.jenetics.*;
+import io.jenetics.engine.*;
+import java.util.List;
+
+public class MatchingEngine {
+    private List<HousingOption> housingOptions; 
+    public MatchingEngine(List<HousingOption> housingOptions) {
+        this.housingOptions = housingOptions;
+    }
+    
+}
