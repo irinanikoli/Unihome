669d3c7af7432340c12ba9db3c4fb9329e23ed53
Aimilia <t8230038@gmail.gr>
2024-11-28 16:56:42 +0200

    unfinished class InputValidator

--- Criteria.java
+++ Criteria.java
@@ -5,29 +5,73 @@ import java.util.Map;
 public class Criteria {
     String stName;
     String stUniversity;
-    double budget;
-    double sqMeters;
+    double maxBudget;
+    double minSqMeters;
+    double maxDistanceFromUni;
+    double maxDistanceFromMeans;
+    @SuppressWarnings({ "rawtypes", "unchecked" })
     Map <String,Integer> preferences = new HashMap();
-    public Criteria(String aname, String auniveristy, double abudget, double asqMeters) {
+    public Criteria(String aname, String auniveristy, double abudget, double asqMeters, double amaxDistanceFromUni, double amaxDistanceFromMeans) {
         stName = aname;
         stUniversity = auniveristy;
-        budget = abudget;
-        sqMeters = asqMeters;
+        maxBudget = abudget;
+        minSqMeters = asqMeters;
+        maxDistanceFromUni = amaxDistanceFromUni;
+        maxDistanceFromMeans = amaxDistanceFromMeans;
     }
-    public void setBudget(double newBudget) {
-        budget = newBudget;
+
+    public String getStUniversity() {
+        return stUniversity;
     }
+
+    public void setStUniversity(String newStUniversity) {
+        stUniversity = newStUniversity;
+    }
+
     public double getBudget() {
-        return budget;
+        return maxBudget;
     }
+
+    public void setBudget(double newBudget) {
+        maxBudget = newBudget;
+    }
+
+    public double getMinSqMeters() {
+        return minSqMeters;
+    }
+
+    public void setMinSqMeters(double newMinSqMeters) {
+        minSqMeters = newMinSqMeters;
+    }
+
+    public double getMaxDistanceFromUni() {
+        return maxDistanceFromUni;
+    }
+
+    public void setMaxDistanceFromUni(double newMaxDistanceFromUni) {
+        maxDistanceFromUni = newMaxDistanceFromUni;
+    }
+
+    public double getMaxDistanceFromMeans() {
+        return maxDistanceFromMeans;
+    }
+
+    public void setMaxDistanceFromMeans(double newMaxDistanceFromMeans) {
+        maxDistanceFromMeans = newMaxDistanceFromMeans;
+    }
+
     public void setPreferences(String criteria, int prOrder) {
         preferences.put(criteria, prOrder);
     }
+
+    @SuppressWarnings("rawtypes")
     public Map getPreferences() {
         return preferences;
     }
+
     @Override
     public String toString() {
-            return "Όνομα:" + stName + ",Πανεπιστήμιο:" + stUniversity + ",Budget:" + budget + ",Επιθυμητή επιφάνεια σπιτιού:" + sqMeters;
+            return "Όνομα:" + stName + ",Πανεπιστήμιο:" + stUniversity + ",Budget:" + maxBudget + ",Επιθυμητή επιφάνεια σπιτιού:" + minSqMeters;
     }
+
 }
