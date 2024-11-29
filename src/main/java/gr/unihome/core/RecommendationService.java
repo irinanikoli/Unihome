15cefe018750a57bd7bb9d25af36db8f7c979325
= <t8230154@aueb.gr>
2024-11-30 00:49:04 +0200

    moving RecommendationService.java to the src path

---
+++ src/main/java/gr/unihome/core/RecommendationService.java
@@ -0,0 +1,34 @@
+import java.util.List;
+import java.util.Map;
+
+
+public class RecommendationService {
+
+    /* A class to recommend houses based on MatchingEngine.java's results
+     * It plays the connecting role between other classes and MatchingEngine
+     * Returns the best house and the list of recommendations for the student    
+     */
+
+    private MatchingEngine matchingEngine;
+    public RecommendationService(List<HousingOption> housingOptions, Map<String, Double> weights) {
+        //initialization based on the list of housing and student's weights of preference
+        this.matchingEngine = new MatchingEngine(housingOptions, weights);
+    }
+
+    //Provides the best house for each student
+    public HousingOption getBestHouse(Criteria studentCriteria) {
+        return matchingEngine.optimize(studentCriteria);
+    } 
+    /*
+     * Provides the list with the best recommendations
+     * @param studentCriteria as the criteria of each student
+     * @param treshold as the limit of divergence between maxScore and scores of the recommended houses
+     * @return list of recommedations  
+     */
+
+    public List<HousingOption> getBestRecommendationList(Criteria studentCriteria, double treshold) {
+        return matchingEngine.findOtherBestSolutions(studentCriteria, treshold);
+    } 
+
+    
+}
