122b90860a0c9107e70f34835c9dc3bf6ae1f6a1
= <t8230154@aueb.gr>
2024-11-30 17:53:18 +0200

    fixing and adding AppLogger to the correct paht

---
+++ src/main/java/gr/unihome/core/AppLogger.java
@@ -0,0 +1,31 @@
+package gr.unihome.core;
+
+import java.util.logging.*;
+import java.io.IOException;
+
+public class AppLogger {
+    /*
+     * Uses Logger class from java.io.logging
+     * A class used by all clases to record every error that occurs during the making
+     * Singleton: Only 1 instance of this class will be used
+     */
+    private static final Logger logger = Logger.getLogger("GlobalLogger");
+
+    static {
+        //setting the "mistakes" file
+        try {
+            FileHandler filehandler = new FileHandler("application.log", true);
+            filehandler.setFormatter(new SimpleFormatter());
+            logger.addHandler(filehandler);
+            ConsoleHandler consoleHandler = new ConsoleHandler();
+            consoleHandler.setLevel(Level.ALL);
+
+        } catch(IOException e) {
+            System.out.println("Error setting up filehandler" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    public static Logger getLogger() {
+        return logger;
+    }
+}
