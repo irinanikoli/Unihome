56473a940dacfe6db80fe5ec131a34b9db6e3528
= <t8230154@aueb.gr>
2024-12-04 20:39:32 +0200

    adding junit imports in MatchingEngineTest

--- src/test/java/gr/unihome/core/MatcingEngineTest.java
+++ src/test/java/gr/unihome/core/MatcingEngineTest.java
@@ -1,5 +1,12 @@
 package gr.unihome.core;
 
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import static org.junit.Assert.*;
+
 public class MatcingEngineTest {
     
 }
