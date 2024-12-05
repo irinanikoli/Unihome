678f7654436eac21321c2cb85bef563e53ddd8f1
= <t8230154@aueb.gr>
2024-12-05 21:52:31 +0200

    Deleting useless import

--- src/main/java/gr/unihome/core/HousingOption.java
+++ src/main/java/gr/unihome/core/HousingOption.java
@@ -3,7 +3,6 @@ package gr.unihome.core;
 import java.util.ArrayList;
 import java.util.List;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
