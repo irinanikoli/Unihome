f5eca9abf49578b430047e1c221d5b846bd4fbe4
= <t8230154@aueb.gr>
2024-12-16 19:35:10 +0200

    Correcting MatchingEngineTest.java name

--- src/test/java/gr/unihome/core/MatchingEngineTest.java
+++ src/test/java/gr/unihome/core/MatchingEngineTest.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-public class MatcingEngineTest {
+public class MatchingEngineTest {
     private MatchingEngine matchingEngine;
     private List<HousingOption> housingOptions;
     private Criteria student ;
