5a086bee847b51b7daf83029f7c6035769aa5d2c
= <t8230154@aueb.gr>
2024-12-30 19:45:43 +0200

    forming main

--- src/main/java/gr/unihome/core/Application.java
+++ src/main/java/gr/unihome/core/Application.java
@@ -1,11 +1,38 @@
 package gr.unihome.core;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
-                
+    static int counter = 1;
+    private static String budget = "";
+    private static String mintm = "";
+    private static String maxtm = "";
+    private static String uniDistance = "";
+    private static String metroDistance = "";
+    private static String budget_preference = "";
+    private static String tm_preference = ""; 
+    private static String unidistance_preference = "";
+    private static String mmmdistance_preference = "";
+    public static void main(String[] args) throws IOException {
+        SpringApplication.run(Application.class, args);
+        if (counter == 1) {
+            HousesDatabase.initialize();
+            UniversitiesDatabase.initialize();
+            MeansDatabase.initialize();
+
+            HousesDatabase.insertRandomHouses(100);
+            UniversitiesDatabase.insertRandomUniversities(8);
+            MeansDatabase.insertRandomMeans(50);
+        }
+        counter++;
+
 
     }
 
