a4002c2c0ec2e2a7c649019225f522caa18ff9c1
= <t8230154@aueb.gr>
2025-01-09 13:50:16 +0200

    deleting useless imports

--- src/main/java/gr/unihome/core/Application.java
+++ src/main/java/gr/unihome/core/Application.java
@@ -3,8 +3,7 @@ package gr.unihome.core;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
+
 
 public class Application {
     public static void main(String[] args) {
